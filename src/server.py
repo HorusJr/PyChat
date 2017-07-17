6452d972c697cbebdd5ccc190b454bb01709e954
Biraj Dahal <bdahal@g.clemson.edu>
2017-07-17 19:01:26 -0400

    Made good server

--- src/server.py
+++ src/server.py
@@ -2,30 +2,35 @@
 
 import asyncio
 import websockets
+import logging
 
-connections = set()
+clients = {}
 
 async def handler(websocket, path):
-    global connected
-    print(websocket)
-    # Register
-    connections.add(websocket)
-    connected = True
+    print('New client', websocket)
+    print(' ({} existing clients)'.format(len(clients)))
 
-    while connected:
-        size = len(connections)
-        try:
-            # Implement logic here.
-            print("Ping: " + str(size))
-            await asyncio.wait([ws.send("Ping: " + str(size)) for ws in connections])
-            await asyncio.sleep(5)
-        except websockets.exceptions.ConnectionClosed as e:
-            print("EXCEPTION!!!")
-            connected = False
-        except:
-            raise
+    # first line from client is client name
+    name = await websocket.recv()
+    await websocket.send('Welcome to the chatroom, {}'.format(name))
+    await websocket.send('Users: {}'.format(list(clients.values())))
+    clients[websocket] = name
+    for client, _ in clients.items():
+        await client.send(name + ' has joined the chat')
 
-    connected.remove(websocket)
+    while True:
+        message = await websocket.recv()
+        if message is None:
+            their_name = clients[websocket]
+            del clients[websocket]
+            print('Client closed connection', websocket)
+
+            for client, _ in clients.items():
+                await client.send(their_name + ' has let the chat')
+            break
+
+        for client, _ in clients.items():
+            await client.send('{}: {}'.format(name, message))
 
 start_server = websockets.serve(handler, 'localhost', 3000, timeout=100) #put the server's local ip in here
 
